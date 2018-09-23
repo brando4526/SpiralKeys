fc4b5836145305266faa5e1bfdec484a68d46a00
Brandon Lovrien <brando4526@gmail.com>
2018-09-22 20:05:49 -0700

    Created user control to handle text prediction options.

---
+++ SpiralKeys/UIControls/PredictionSelector.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace SpiralKeys.UIControls
+{
+    /// <summary>
+    /// Interaction logic for PredictionSelector.xaml
+    /// </summary>
+    public partial class PredictionSelector : UserControl
+    {
+        private List<string> predictionOptions;
+
+        public PredictionSelector()
+        {
+            InitializeComponent();
+            predictionOptions = new List<string> { "optionone", "optiontwo", "optionthree", "optionfour" };
+            PredictionListBox.ItemsSource = predictionOptions;
+        }
+    }
+}
